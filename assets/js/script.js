ae4befa136a1f5730ebe4b48956241aaff061270
Andrew Wilson <Wilsonandrew57@gmail.com>
2021-07-01 21:17:04 -0500

    works until UV index, needs local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,10 +21,61 @@ var day2 = document.querySelector('.day2');
 var day3 = document.querySelector('.day3');
 var day4 = document.querySelector('.day4');
 var day5 = document.querySelector('.day5');
+// api 
+var currWeatherStart = 'https://api.openweathermap.org/data/2.5/weather?q=';
+var uvStart = 'http://api.openweathermap.org/data/2.5/solar_radiation?lat=';
+var uvMid = '&lon=';
+var searchedCity = 'Austin';
+// *api key ccadae41918cefef966b68447d483afa
+var apiKey = '&appid=ccadae41918cefef966b68447d483afa';
+var weatherData;
+var uvIndex;
+var lat = '';
+var lon = '';
+// when search btn is pressed
+searchBtn.addEventListener('click', function (event) {
+    var api = currWeatherStart + searchedCity + apiKey;
+    fetch(api, {})
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+            weatherData = data;
+            getUV(weatherData);
+        })
+})
 
+function displayInfo(data) {
+    lrgDisplay.style.display = 'inline';
+    currCity.textContent = data.name;
+    temp.textContent = 'Temp: ' + Math.round((data.main.temp - 273.15) * 9/5 + 32) * 1 + String.fromCharCode(176) + 'F';
+    wind.textContent = 'Wind: ' + Math.round(data.wind.speed * 2.237) * 1  + ' MPH';
+    humid.textContent = 'Humidity: ' + data.main.humidity + '%';
+    UV.textContent = 'UV Index: '; 
+}
+// !need the correct api this does not work!
+function getUV(data) {
+    lat = data.coord.lat.toString();
+    lon = data.coord.lon.toString();
+    var api = uvStart + lat + uvMid + lon + apiKey;
+    fetch(api, {})
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+
+            
+})
+
+}
+
+// loads on start or reload
 function init() {
     recentList.style.display = 'none';
     lrgDisplay.style.display = 'none';
 }
 
+// when page loads 
 init();
